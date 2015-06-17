95db26eafb8434c49f8cd776e9600e1fdb06497e
alex <alexandrosrotsides@gmail.com>
2015-06-17 22:08:13 +0100

    improved conversion of elements

--- app/src/main/rs/pixelsCalc.rs
+++ app/src/main/rs/pixelsCalc.rs
@@ -25,59 +25,39 @@ void root(const uchar4 *in, uchar4 *out, uint32_t x, uint32_t y) {
     }else{ //do image processing here
 
         //top left pixel
-        float4 elementF4TL = rsUnpackColor8888(*(uchar*)rsGetElementAt(inPixels, x-1, y-1));
-        uchar4 pixelCharsTL = rsPackColorTo8888(elementF4TL);
-        //float3 pixelTL = convert_float4(pixelCharsTL).rgb;
-
         float3 pixelTL = convert_float4(rsGetElementAt_uchar4(inPixels, x-1, y-1)).rgb;
         threeBythree[0] = (pixelTL.r + pixelTL.g + pixelTL.b)/3;
 
         //upper middle pixel
-        float4 elementF4UM = rsUnpackColor8888(*(uchar*)rsGetElementAt(inPixels, x, y-1));
-        uchar4 pixelCharsUM = rsPackColorTo8888(elementF4UM);
-        float3 pixelUM = convert_float4(pixelCharsUM).rgb;
+        float3 pixelUM = convert_float4(rsGetElementAt_uchar4(inPixels, x, y-1)).rgb;
         threeBythree[1] = (pixelUM.r + pixelUM.g + pixelUM.b)/3;
 
         //top right pixel
-        float4 elementF4TR = rsUnpackColor8888(*(uchar*)rsGetElementAt(inPixels, x+1, y+1));
-        uchar4 pixelCharsTR = rsPackColorTo8888(elementF4TR);
-        float3 pixelTR = convert_float4(pixelCharsTR).rgb;
+        float3 pixelTR = convert_float4(rsGetElementAt_uchar4(inPixels,  x+1, y+1)).rgb;
         threeBythree[2] = (pixelTR.r + pixelTR.g + pixelTR.b)/3;
 
         // middle left pixel
-        float4 elementF4ML = rsUnpackColor8888(*(uchar*)rsGetElementAt(inPixels, x-1, y));
-        uchar4 pixelCharsML = rsPackColorTo8888(elementF4ML);
-        float3 pixelML = convert_float4(pixelCharsML).rgb;
+        float3 pixelML = convert_float4(rsGetElementAt_uchar4(inPixels,  x-1, y)).rgb;
         threeBythree[3] = (pixelML.r + pixelML.g + pixelML.b)/3;
 
         //middle right pixel
-        float4 elementF4MR = rsUnpackColor8888(*(uchar*)rsGetElementAt(inPixels, x+1, y));
-        uchar4 pixelCharsMR = rsPackColorTo8888(elementF4MR);
-        float3 pixelMR = convert_float4(pixelCharsMR).rgb;
+        float3 pixelMR = convert_float4(rsGetElementAt_uchar4(inPixels, x+1, y)).rgb;
         threeBythree[4] = (pixelMR.r + pixelMR.g + pixelMR.b)/3;
 
         //middle center pixel (the same as pixel)
-        float4 elementF4MC = rsUnpackColor8888(*(uchar*)rsGetElementAt(inPixels, x, y));
-        uchar4 pixelCharsMC = rsPackColorTo8888(elementF4MC);
-        float3 pixelMC = convert_float4(pixelCharsMC).rgb;
+        float3 pixelMC = convert_float4(rsGetElementAt_uchar4(inPixels, x, y)).rgb;
         threeBythree[5] = (pixelMC.r + pixelMC.g + pixelMC.b)/3;
 
         //bottom left pixel
-        float4 elementF4BL = rsUnpackColor8888(*(uchar*)rsGetElementAt(inPixels, x-1, y+1));
-        uchar4 pixelCharsBL = rsPackColorTo8888(elementF4BL);
-        float3 pixelBL = convert_float4(pixelCharsBL).rgb;
+        float3 pixelBL = convert_float4(rsGetElementAt_uchar4(inPixels, x-1, y+1)).rgb;
         threeBythree[6] = (pixelBL.r + pixelBL.g + pixelBL.b)/3;
 
         //bottom middle left pixel
-        float4 elementF4BML = rsUnpackColor8888(*(uchar*)rsGetElementAt(inPixels, x, y+1));
-        uchar4 pixelCharsBML = rsPackColorTo8888(elementF4BML);
-        float3 pixelBML = convert_float4(pixelCharsBML).rgb;
+        float3 pixelBML = convert_float4(rsGetElementAt_uchar4(inPixels, x, y+1)).rgb;
         threeBythree[7] = (pixelBML.r + pixelBML.g + pixelBML.b)/3;
 
         //bottom right pixel
-        float4 elementF4BR = rsUnpackColor8888(*(uchar*)rsGetElementAt(inPixels, x+1, y+1));
-        uchar4 pixelCharsBR = rsPackColorTo8888(elementF4BR);
-        float3 pixelBR = convert_float4(pixelCharsBR).rgb;
+        float3 pixelBR = convert_float4(rsGetElementAt_uchar4(inPixels, x+1, y+1)).rgb;
         threeBythree[8] = (pixelBR.r + pixelBR.g + pixelBR.b)/3;
 
         //pixel.r = (pixelTL.r + pixelUM.r + pixelTR.r + pixelML.r + pixelMR.r + pixelBL.r + pixelBML.r + pixelBR.r)/8;
