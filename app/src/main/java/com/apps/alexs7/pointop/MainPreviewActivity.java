145cf7bbdb099e17ff35172597b23d52bc6f12f0
alex <alexandrosrotsides@gmail.com>
2015-07-25 18:37:55 +0100

    removed controls fragment

--- app/src/main/java/com/apps/alexs7/pointop/MainPreviewActivity.java
+++ app/src/main/java/com/apps/alexs7/pointop/MainPreviewActivity.java
@@ -28,7 +28,6 @@ public class MainPreviewActivity extends Activity {
 
         Fragment cleanPreviewFragment = fragmentManager.findFragmentById(R.id.clean_preview_fragment);
         cleanPreviewFragment.getView().setBackgroundColor(Color.BLACK);
-        Fragment controlsFragment = fragmentManager.findFragmentById(R.id.controls_fragment);
         Fragment processedPreviewFragment = fragmentManager.findFragmentById(R.id.processed_preview_fragment);
         super.onStart();
     }
